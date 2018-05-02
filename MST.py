73c764a670c2c9bb13ff78023c99965e85272886
Brian <cobo.brian97@gmail.com>
2018-05-01 22:30:45 -0500

    Add files via upload
    File to access either Prim or Kruskal

---
+++ MST.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 30 13:35:49 2018
+
+@author: brian
+"""
+
+from Prims_Algorithm import Prims
+#from Kurskals...
+
+def MST(textfile, method = 'Prims', starting_vertex=0, show=False):
+    
+    if method == 'Prims':
+        return Prims(textfile, starting_vertex, show)
+    
