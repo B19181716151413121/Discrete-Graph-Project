60c6d2bc8f829ac52db373f392ee1d801a9a95be
Brian <cobo.brian97@gmail.com>
2018-05-03 10:21:05 -0500

    Add files via upload

--- MST.py
+++ MST.py
@@ -9,8 +9,8 @@ Created on Mon Apr 30 13:35:49 2018
 from Prims_Algorithm import Prims
 #from Kurskals...
 
-def MST(textfile, method = 'Prims', starting_vertex=0, show=False):
+def MST(textfile, method = 'Prims', starting_vertex=0, show_cost = False, show_path = False, show=False):
     
     if method == 'Prims':
-        return Prims(textfile, starting_vertex, show)
+        return Prims(textfile, starting_vertex, show_cost, show_path, show)
     
