c6d3996a66643a22bba6b1274dd97edf5bffa485
Brian <cobo.brian97@gmail.com>
2018-05-03 10:18:32 -0500

    Add files via upload

--- Prims_Algorithm.py
+++ Prims_Algorithm.py
@@ -6,22 +6,24 @@ Created on Mon Apr 30 13:36:53 2018
 @author: brian
 """
 
-from prims_functions import cost, update_tree, initialize_tree
+from prims_functions import cost, update_tree, initialize_tree, printPath
 from Weighted_Graph import *
 
-
-
-def Prims(textfile, starting_vertex = 0, show_cost = False, show = False):
+def Prims(textfile, starting_vertex = 0, show_cost = False, show_path = False, show = False):
     G = Weighted_Graph(textfile)
-    
     T = initialize_tree(starting_vertex)
+    
     if show == True:
         G.draw_graph()
-        G.draw_subgraph(T)
+        #G.draw_subgraph(T)
+    
     while T[0] != G.vertex_set():
         update_tree(G, T)
         if show == True:
             G.draw_subgraph(T)
+    
+    if show_path == True:
+        printPath()    
         
     if show_cost == True:
         c = 0
@@ -30,3 +32,5 @@ def Prims(textfile, starting_vertex = 0, show_cost = False, show = False):
         print('Optimal tree cost:', c)
         
     return T
+
+#Prims('graph4.txt', starting_vertex = 0, show_cost = True, show_path = True, show = True)
